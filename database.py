b0191f65df0555f9872995ac92e3942f16b7c75b
Damir Suleymanov <damir@cloudlock.com>
2017-09-24 13:33:24 -0400

    Encrypt emails in the database

--- database.py
+++ database.py
@@ -1,12 +1,19 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import EncryptedType
+
 db = SQLAlchemy()
 
 
 class Subscriber(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), unique=True)
+    email = db.Column(EncryptedType(db.String(255), get_secret_key()), unique=True)
     city = db.Column(db.String(255))
 
     def __init__(self, email, city):
         self.email = email
         self.city = city
+
+
+def get_secret_key():
+    return os.getenv('EMAIL_ENCRYPTION_KEY') or "secret_key"
