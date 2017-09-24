8640a391d5a08710fdcd7da740516a1f7fe54d2b
Damir Suleymanov <damir@cloudlock.com>
2017-09-24 13:16:06 -0400

    Handle IntegrityError

--- subscribers.py
+++ subscribers.py
@@ -1,7 +1,20 @@
+import logging
+
+from sqlalchemy.exc import IntegrityError
+
 from database import Subscriber, db
+
+
+class SubscriptionError(Exception):
+    pass
 
 
 def add_subscriber(email, location):
     subscriber = Subscriber(email, location)
     db.session.add(subscriber)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except IntegrityError, e:
+        logger = logging.getLogger(__name__)
+        logger.exception("Failed to add a subscriber")
+        raise SubscriptionError("Subscriber with this email already exists")
