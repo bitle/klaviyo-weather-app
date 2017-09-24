072c3550aa3b7dd18b18d6bb27f4ac75b0693490
Damir Suleymanov <damir@cloudlock.com>
2017-09-24 13:17:23 -0400

    Handler subscription error in form handler

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request
 
 from cities import get_cities
-from subscribers import add_subscriber
+from subscribers import add_subscriber, SubscriptionError
 
 status_apis = Blueprint('status_apis', __name__)
 forms = Blueprint('forms', __name__, template_folder='templates')
@@ -13,8 +13,8 @@ def status():
 
 
 @forms.route('/')
-def signup_form():
-    return render_template("signup.html", cities=get_cities())
+def signup_form(error=None):
+    return render_template("signup.html", cities=get_cities(), error=error)
 
 
 @forms.route('/', methods=['POST'])
@@ -22,6 +22,9 @@ def signup_form_submit():
     email = request.form['email']
     location = request.form['location']
 
-    add_subscriber(email, location)
+    try:
+        add_subscriber(email, location)
+    except SubscriptionError:
+        return signup_form(error="Subscriber with this email already exists")
 
     return signup_form()
