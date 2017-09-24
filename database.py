e2f96ada1982f58caf74ae4f69ab8d8831bfeafb
Damir Suleymanov <damir@cloudlock.com>
2017-09-24 11:07:57 -0400

    Move models into database module

--- database.py
+++ database.py
@@ -1,2 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+
+class Subscriber(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    city = db.Column(db.String(255))
+
+    def __init__(self, email, city):
+        self.email = email
+        self.city = city
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from app import create_app
+from database import db
 
 
 if __name__ == "__main__":
     # Running in debug mode
     app = create_app(debug=True)
+    db.create_all(app=app)
     app.run()
 else:
     # Running from gunicorn
