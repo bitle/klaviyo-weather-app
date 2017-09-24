749c7bd0bf7d459310e277f679b91fd8ab7e7273
Damir Suleymanov <damir@cloudlock.com>
2017-09-23 22:17:53 -0400

    Add database connection

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
 from views import status_apis
+from database import db
 
 
 def create_app(debug=False):
     app = Flask(__name__)
     app.debug = debug
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI',
+                                                      'postgres://localhost:5432/klaviyo')
+    # SQLALCHEMY_TRACK_MODIFICATIONS adds significant overhead
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.register_blueprint(status_apis)
+    db.init_app(app)
 
     return app
