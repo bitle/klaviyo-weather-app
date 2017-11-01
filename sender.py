c94443c5c6268f4772d83735b4e0534fd06cf2c5
Damir Suleymanov <damir@cloudlock.com>
2017-11-01 14:30:31 +0300

    Add mailgun support

--- sender.py
+++ sender.py
@@ -1,3 +1,8 @@
+import os
+
+import requests
+from requests.auth import HTTPBasicAuth
+
 from app import create_app
 from database import Subscriber
 from wunderground import get_current_conditions, get_average_temperature
@@ -33,10 +38,46 @@ def send_email_console(email, location):
     print "%s - %s\n\t%s" % (email, subject, body)
 
 
+def send_email_mailgun(email, location):
+    base_url = "https://api.mailgun.net/v3/sandbox38be6e72fd2d416a900adad83bdf4bda.mailgun.org"
+    api_key = os.getenv('MAILGUN_API_KEY')
+    email_from = 'damir.suleymanov@gmail.com'
+
+    if not api_key:
+        raise ValueError("Please provide MAILGUN_API_KEY")
+
+    city, state = location.split(', ')
+    subject, body = create_email(city, state)
+
+    email_data = {
+        'from': email_from,
+        'to': email,
+        'subject': subject,
+        'text': body,
+        # 'o:testmode': 'yes'
+    }
+
+    url = "%s/messages" % base_url
+    response = requests.post(url, auth=HTTPBasicAuth("api", api_key),
+                             data=email_data)
+    if response.status_code == 400:
+        print "Bad Request - Often missing a required parameter"
+    elif response.status_code == 401:
+        print "Unauthorized - No valid API key provided"
+    elif response.status_code == 402:
+        print "Request Failed - Parameters were valid but request failed"
+    elif response.status_code == 404:
+        print "Not Found - The requested item doesn't exist"
+    elif response.status_code in [500, 502, 503, 504]:
+        print "Server Errors - something is wrong on Mailgun's end"
+
+    # print response.text
+
+
 def send_emails():
     # I need this Flask app, because I made a mistake of using Flask-Sqlalchemy
     flask_app = create_app()
     with flask_app.app_context():
         subscribers = Subscriber.query.all()
         for subscriber in subscribers:
-            send_email_console(subscriber.email, subscriber.city)
+            send_email_mailgun(subscriber.email, subscriber.city)
